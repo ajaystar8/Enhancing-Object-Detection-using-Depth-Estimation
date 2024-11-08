e5362e656b4d082e6b9922fba7414c90cd22c6ca
ajaystar8 <ajayrajendrakumar8@gmail.com>
2024-11-08 11:38:12 -0500

    adapt code from source

--- dataset.py
+++ dataset.py
@@ -1,14 +1,17 @@
 import os
-import config
 
 import numpy as np
 import pandas as pd
 import torch
 from PIL import Image, ImageFile
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
+import config
 from utils import (
-    iou_width_height as iou
+    cells_to_bboxes,
+    iou_width_height as iou,
+    non_max_suppression as nms,
+    plot_image
 )
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -88,3 +91,37 @@ class YOLODataset(Dataset):
 
         return image, tuple(targets)
 
+
+def test():
+    anchors = config.ANCHORS
+
+    transform = config.train_transforms
+
+    dataset = YOLODataset(
+        config.DATASET + '/train.csv',
+        config.IMG_DIR,
+        config.LABEL_DIR,
+        S=[13, 26, 52],
+        anchors=anchors,
+        transform=transform,
+    )
+    S = [13, 26, 52]
+    scaled_anchors = torch.tensor(anchors) / (
+            1 / torch.tensor(S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
+    )
+    loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
+    for x, y in loader:
+        boxes = []
+
+        for i in range(y[0].shape[1]):
+            anchor = scaled_anchors[i]
+            boxes += cells_to_bboxes(
+                y[i], is_preds=False, S=y[i].shape[2], anchors=anchor
+            )[0]
+        boxes = nms(boxes, iou_threshold=1, threshold=0.7, box_format="midpoint")
+        print(boxes)
+        plot_image(x[0].permute(1, 2, 0).to("cpu"), boxes)
+
+
+if __name__ == "__main__":
+    test()
